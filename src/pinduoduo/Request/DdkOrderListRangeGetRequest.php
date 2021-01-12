a2755cebf4ed0d634c18dcc8f333bce0e87331b6
李顺 <1185288810@qq.com>
2021-01-12 19:21:58 +0800

    添加字段

--- src/pinduoduo/Request/DdkOrderListRangeGetRequest.php
+++ src/pinduoduo/Request/DdkOrderListRangeGetRequest.php
@@ -1,81 +1,94 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: manbinzheng
- * Date: 2020-03-17
- * Time: 12:12
- */
-
-namespace Dml\EasyTBK\PinDuoDuo\Request;
-
-use Dml\EasyTBK\PinDuoDuo\RequestInterface;
-
-
-class DdkOrderListRangeGetRequest implements RequestInterface
-{
     /**
-     * 根据支付时间段查询推广订单接口
-     * @var string
+     * Created by PhpStorm.
+     * User: manbinzheng
+     * Date: 2020-03-17
+     * Time: 12:12
      */
-    private $type = 'pdd.ddk.order.list.range.get';
 
-    private $startPayTime;
+    namespace Dml\EasyTBK\PinDuoDuo\Request;
 
-    private $endPayTime;
+    use Dml\EasyTBK\PinDuoDuo\RequestInterface;
 
-    private $pageSize;
 
-    private $lastOrderId;
-
-    public function setStartPayTime($startPayTime)
+    class DdkOrderListRangeGetRequest implements RequestInterface
     {
-        $this->startPayTime = $startPayTime;
-    }
+        /**
+         * 根据支付时间段查询推广订单接口
+         * @var string
+         */
+        private $type = 'pdd.ddk.order.list.range.get';
 
-    public function getStartPayTime()
-    {
-        return $this->startPayTime;
-    }
+        /**
+         * @JsonProperty(String, "end_time")
+         */
+        private $endTime;
 
-    public function setEndPayTime($endPayTime)
-    {
-        $this->endPayTime = $endPayTime;
-    }
+        /**
+         * @JsonProperty(String, "last_order_id")
+         */
+        private $lastOrderId;
 
-    public function getEndPayTime()
-    {
-        return $this->endPayTime;
-    }
+        /**
+         * @JsonProperty(Integer, "page_size")
+         */
+        private $pageSize;
 
-    public function setPageSize($pageSize)
-    {
-        $this->pageSize = $pageSize;
-    }
+        /**
+         * @JsonProperty(Integer, "query_order_type")
+         */
+        private $queryOrderType;
 
-    public function getPageSize()
-    {
-        return $this->pageSize;
-    }
+        /**
+         * @JsonProperty(String, "start_time")
+         */
+        private $startTime;
 
-    public function setLastOrderId($lastOrderId)
-    {
-        $this->lastOrderId = $lastOrderId;
-    }
+        /**
+         * @JsonProperty(BOOLEAN, "cash_gift_order")
+         */
+        private $cashGiftOrder;
 
-    public function getLastOrderId($lastOrderId)
-    {
-        return $this->lastOrderId;
-    }
+        public function setEndTime($endTime)
+        {
+            $this->endTime = $endTime;
+        }
 
-    public function getParams()
-    {
-        $params = [
-            'type' => $this->type,
-            'start_time' => $this->startPayTime,
-            'end_time' => $this->endPayTime,
-            'page_size' => $this->pageSize,
-            'last_order_id' => $this->lastOrderId
-        ];
-        return array_filter($params);
+        public function setLastOrderId($lastOrderId)
+        {
+            $this->lastOrderId = $lastOrderId;
+        }
+
+        public function setPageSize($pageSize)
+        {
+            $this->pageSize = $pageSize;
+        }
+
+        public function setQueryOrderType($queryOrderType)
+        {
+            $this->queryOrderType = $queryOrderType;
+        }
+
+        public function setStartTime($startTime)
+        {
+            $this->startTime = $startTime;
+        }
+
+        public function setCashGiftOrder($cashGiftOrder)
+        {
+            $this->cashGiftOrder = $cashGiftOrder;
+        }
+        public function getParams()
+        {
+            $params = [
+                'type' => $this->type,
+                'end_time' => $this->endTime,
+                'last_order_id' => $this->lastOrderId,
+                'page_size' => $this->pageSize,
+                'query_order_type' => $this->queryOrderType,
+                'start_time' => $this->startTime,
+                'cash_gift_order' => $this->cashGiftOrder,
+            ];
+            return array_filter($params);
+        }
     }
-}
