36d02a15e4e23cad8995cc92b7102d89c4217ee2
李顺 <1185288810@qq.com>
2021-01-12 19:07:10 +0800

    修改获取订单接口

--- src/pinduoduo/Request/DdkOrderListIncrementGetRequest.php
+++ src/pinduoduo/Request/DdkOrderListIncrementGetRequest.php
@@ -1,76 +1,101 @@
 <?php
 
-namespace Dml\EasyTBK\PinDuoDuo\Request;
+    namespace Dml\EasyTBK\PinDuoDuo\Request;
 
-use Dml\EasyTBK\PinDuoDuo\RequestInterface;
-
-
-class DdkOrderListIncrementGetRequest implements RequestInterface
-{
-    /**
-     * 按照时间段获取授权多多客下面所有多多客的推广订单信息
-     * @var string
-     */
-    private $type = 'pdd.ddk.order.list.increment.get';
-
-    private $startUpdateTime;
-
-    private $endUpdateTime;
-
-    private $pageSize;
-
-    private $page;
+    use Dml\EasyTBK\PinDuoDuo\RequestInterface;
 
 
-    public function setStartUpdateTime($startUpdateTime)
+    class DdkOrderListIncrementGetRequest implements RequestInterface
     {
-        $this->startUpdateTime = $startUpdateTime;
-    }
+        /**
+         * 按照时间段获取授权多多客下面所有多多客的推广订单信息
+         * @var string
+         */
+        private $type = 'pdd.ddk.order.list.increment.get';
 
-    public function getStartUpdateTime()
-    {
-        return $this->startUpdateTime;
-    }
+        /**
+         * @JsonProperty(BOOLEAN, "cash_gift_order")
+         */
+        private $cashGiftOrder;
 
-    public function setEndUpdateTime($endUpdateTime)
-    {
-        $this->endUpdateTime = $endUpdateTime;
-    }
 
-    public function getEndUpdateTime()
-    {
-        return $this->endUpdateTime;
-    }
+        /**
+         * @JsonProperty(Long, "end_update_time")
+         */
+        private $endUpdateTime;
 
-    public function setPageSize($pageSize)
-    {
-        $this->pageSize = $pageSize;
-    }
+        /**
+         * @JsonProperty(Integer, "page")
+         */
+        private $page;
 
-    public function getPageSize()
-    {
-        return $this->pageSize;
-    }
+        /**
+         * @JsonProperty(Integer, "page_size")
+         */
+        private $pageSize;
 
-    public function setPage($page)
-    {
-        $this->page = $page;
-    }
+        /**
+         * @JsonProperty(Integer, "query_order_type")
+         */
+        private $queryOrderType;
 
-    public function getPage()
-    {
-        return $this->page;
-    }
+        /**
+         * @JsonProperty(Boolean, "return_count")
+         */
+        private $returnCount;
 
-    public function getParams()
-    {
-        $params = [
-            'type' => $this->type,
-            'start_update_time' => $this->startUpdateTime,
-            'end_update_time' => $this->endUpdateTime,
-            'page_size' => $this->pageSize,
-            'page' => $this->page
-        ];
-        return array_filter($params);
+        /**
+         * @JsonProperty(Long, "start_update_time")
+         */
+        private $startUpdateTime;
+
+        public function setcashGiftOrder($cashGiftOrder)
+        {
+            $this->cashGiftOrder = $cashGiftOrder;
+        }
+
+        public function setEndUpdateTime($endUpdateTime)
+        {
+            $this->endUpdateTime = $endUpdateTime;
+        }
+
+        public function setPage($page)
+        {
+            $this->page = $page;
+        }
+
+        public function setPageSize($pageSize)
+        {
+            $this->pageSize = $pageSize;
+        }
+
+        public function setQueryOrderType($queryOrderType)
+        {
+            $this->queryOrderType = $queryOrderType;
+        }
+
+        public function setReturnCount($returnCount)
+        {
+            $this->returnCount = $returnCount;
+        }
+
+        public function setStartUpdateTime($startUpdateTime)
+        {
+            $this->startUpdateTime = $startUpdateTime;
+        }
+
+        public function getParams()
+        {
+            $params = [
+                'type' => $this->type,
+                'cash_gift_order' => $this->cashGiftOrder,
+                'start_update_time' => $this->startUpdateTime,
+                'end_update_time' => $this->endUpdateTime,
+                'page_size' => $this->pageSize,
+                'page' => $this->page,
+                'query_order_type' => $this->queryOrderType,
+                'return_count' => $this->returnCount
+            ];
+            return array_filter($params);
+        }
     }
-}
